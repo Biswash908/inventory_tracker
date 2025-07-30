daea97093f4a60751753d956057af471014979ca
Biswash908 <biswashdem0987@gmail.com>
2025-07-30 21:08:08 +0545

    Fixed vercel issue

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { signIn, signUp } from "@/app/auth/actions";
+import { signIn } from "@/app/auth/actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
